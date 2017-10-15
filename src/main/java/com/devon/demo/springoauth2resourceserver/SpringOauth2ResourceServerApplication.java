07aefdd085fd0efcb8d03e54ac8d754ae2d5c3c9
loudywen <loudywen@gmail.com>
2017-10-15 11:39:57 +0800

    first commit

---
+++ src/main/java/com/devon/demo/springoauth2resourceserver/SpringOauth2ResourceServerApplication.java
@@ -0,0 +1,12 @@
+package com.devon.demo.springoauth2resourceserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauth2ResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauth2ResourceServerApplication.class, args);
+	}
+}
